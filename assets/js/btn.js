3d0377f6288e0a0a8c48aaf72ca9eee1578a1634
Lucas <lac.lucas96@gmail.com>
2022-04-25 19:07:45 -0300

    Add bg style to floatingbtn click

--- assets/js/btn.js
+++ assets/js/btn.js
@@ -1,15 +1,16 @@
 const btn = document.getElementById("floatbtn");
 const options = document.querySelectorAll(".floating__hidden__item");
-
-console.log(options);
+const modal = document.querySelector(".floating-modal");
 
 btn.addEventListener('click', () => {
     options.forEach(element => {
         const visibility = element.getAttribute("data-visible");
         if (visibility == "false") {
             element.setAttribute("data-visible", "true");
+            modal.style.display = 'block';
         } else {
             element.setAttribute("data-visible", "false");
+            modal.style.display = 'none';
         }
     })
 })
